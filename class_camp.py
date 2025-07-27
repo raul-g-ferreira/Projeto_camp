ba88f09ffa2c014d0b066a9fdc4281fddd06e635
raul-g-ferreira <raul.gferreira7@gmail.com>
2025-07-27 20:08:50 -0300

    Done

--- class_camp.py
+++ class_camp.py
@@ -30,8 +30,8 @@ def verifica_nome(nome_alvo: str, lst:list[Clube], inicio: int) -> int:
     Determina o índice de um Clube de nome *nome* em *lst*.
     Retorna -1 caso o clube *nome* não estiver na lista.
     Exemplos:
-    >>> Curitiba = Clube('Curitiba', 0, 0, 0, 0, 0, 0, 0, 0, 0)  
-    >>> Corinthians = Clube('Corinthians', 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    >>> Curitiba = Clube('Curitiba', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)  
+    >>> Corinthians = Clube('Corinthians', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
   
     >>> times = [Curitiba, Corinthians]
     
@@ -51,6 +51,7 @@ def verifica_nome(nome_alvo: str, lst:list[Clube], inicio: int) -> int:
     else:
         idx = verifica_nome(nome_alvo, lst, inicio + 1)
     return idx
+
 def acha_espaco(s: str) -> list[int]:
     '''
     Devolve uma lista com os indices dos espaços de *s*
@@ -63,7 +64,6 @@ def acha_espaco(s: str) -> list[int]:
         if s[i] == ' ':
             indices.append(i)
     return indices
-
 
 def processa_jogos(jogos: list[str]) -> list[Jogo]:
     '''
@@ -128,10 +128,14 @@ def processa_clubes(jogos: list[Jogo]) -> list[Clube]:
     0
     >>> clubes[0].gols_anf
     1
+    >>> clubes[0].draws_anf
+    0
     >>> clubes[0].gols_tomados
     2
     >>> clubes[0].saldo
     -1
+    >>> clubes[0].pontuacao
+    0
     '''
     
     clubes: list[Clube] = []
@@ -205,7 +209,7 @@ def pontuacao_clubes(clubes: list[Clube]):
     - Derrota: 0 pontos
     Sendo armazenada no campo pontuacao de cada clube.
     Exemplo:
-    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0)]
+    >>> clubes = [Clube('Sao-Paulo',1, 1, 0, 2, 1, 1, 2, 0, 1, 0, 0, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0)]
     >>> pontuacao_clubes(clubes)
     >>> clubes[0].pontuacao
     3
@@ -222,25 +226,34 @@ def em_ordem_alfabetica(clubex: Clube, clubey: Clube) -> bool:
     Compara dois clubes e retorna True se o nome do clube *clubex* for
     lexicograficamente menor que o nome do clube *clubey*.
     Exemplo:
-    >>> Sao_Paulo = Clube('Sao-Paulo', 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    >>> Flamengo = Clube('Flamengo', 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    >>> Sao_Paulo = Clube('Sao-Paulo', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    >>> Flamengo = Clube('Flamengo', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     >>> em_ordem_alfabetica(Sao_Paulo, Flamengo)
     True
     >>> em_ordem_alfabetica(Flamengo, Sao_Paulo)
     False
     '''
     return clubex.nome < clubey.nome
+
 def ordena_clubes(clubes: list[Clube]):
     '''
-    Ordena *clubes* de acordo com os seguints critérios:
+    Ordena *clubes* de acordo com os seguintes critérios:
     1. Pontuação
     2. Número de vitórias
     3. Saldo de gols
     4. Ordem alfabética do nome do clube
-    Retorna a lista ordenada.
     Exemplo:
-    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0)]
-    >>> classificacao(clubes)
+    >>> clubes = [
+    ...     Clube('Palmeiras', 2, 1, 1, 3, 1, 1, 2, 1, 1, 2, 4, 0),
+    ...     Clube('Flamengo', 2, 2, 0, 4, 1, 1, 3, 1, 1, 1, 3, 0),
+    ...     Clube('Sao-Paulo', 2, 1, 1, 2, 1, 1, 2, 1, 1, 2, 2, 0)
+    ... ]
+    >>> pontuacao_clubes(clubes)
+    >>> ordena_clubes(clubes)
+    >>> clubes
+    [Clube(nome='Flamengo', jogos_totais=2, wins=2, draws=0, gols_totais=4, jogos_anf=1, wins_anf=1, gols_anf=3, draws_anf=1, gols_tomados=1, saldo=3, pontuacao=6),
+     Clube(nome='Palmeiras', jogos_totais=2, wins=1, draws=1, gols_totais=3, jogos_anf=1, wins_anf=1, gols_anf=2, draws_anf=1, gols_tomados=1, saldo=2, pontuacao=4),
+     Clube(nome='Sao-Paulo', jogos_totais=2, wins=1, draws=1, gols_totais=2, jogos_anf=1, wins_anf=1, gols_anf=2, draws_anf=1, gols_tomados=1, saldo=1, pontuacao=4)]
     '''
     for i in range(len(clubes) - 1):
         for j in range(len(clubes) - 1):
@@ -274,11 +287,15 @@ def mostra_classificacao(clubes: list[Clube]):
     Onde TIME é o nome do clube, P é a pontuação, V é o número de vitórias e S é o saldo de gols.
     Os valores de P, V e S devem ser alinhados verticalmente com os valores dos outros clubes.
     Exemplo:
-    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0), Clube('Palmeiras', 1, 0, 1, 1, 0, 0, 0, 1, 0)]
+    >>> clubes = [
+    Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0, 1, 1, 3),
+    Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1),
+    Clube('Palmeiras', 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1)]
     >>> mostra_classificacao(clubes)
-    TIME      P V S
-    Sao-Paulo 3 1 1
-    Flamengo  1 0 0
+    TIME      P V  S
+    Sao-Paulo 3 1  1
+    Flamengo  1 0  0
+    Palmeiras 1 0  0
     '''
     maior_len = len(clubes[0].nome)
     for clube in clubes:
@@ -293,40 +310,91 @@ def mostra_classificacao(clubes: list[Clube]):
             # print(f"{clube.nome}{' ' * (maior_len - len(clube.nome))} {clube.pontuacao} {clube.wins} {clube.saldo}")
             print(clube.nome + (' ' * (maior_len - len(clube.nome))) + ' ' + str(clube.pontuacao)+ ' ' + str(clube.wins)+ ' ' + str(clube.saldo))
 
-def max_gols_tomados(clubes: list[Clube]) -> int:
+def min_gols_tomados(clubes: list[Clube]) -> int:
     '''
-    Determina o maior número de gols sofridos por um clube.
+    Determina o menor número de gols sofridos por um clube.
     Retorna o número de gols sofridos.
     Exemplo:
-    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0)]
-    >>> mais_gols_tomados(clubes)
-    1
+    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 0, 1, 0, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0)]
+    >>> min_gols_tomados(clubes)
+    0
     '''
-    max_tomados = clubes[0].gols_tomados
+    min_tomados = clubes[0].gols_tomados
     for clube in clubes:
-        if clube.gols_tomados > max_tomados:
-            max_tomados = clube.gols_tomados
-    return max_tomados
+        if clube.gols_tomados < min_tomados:
+            min_tomados = clube.gols_tomados
+    return min_tomados
 
-def pior_defesa(clubes: list[Clube]):
+def melhor_defesa(clubes: list[Clube]):
     '''
-    Imprime o(s) clube(s) com a pior defesa.
-    A pior defesa é o clube que tem o maior número de gols sofridos.
-    Retorna o nome do clube com a pior defesa.
+    Imprime o(s) clube(s) com a defesa menos vazada.
+    A defesa menos vazada é o clube que tem o menor número de gols sofridos.
+    Retorna o nome do clube com a melhor defesa.
     Exemplo:
-    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0)]
-    >>> pior_defesa(clubes)
-    'Flamengo'
+    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0, 0, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0), Clube('Palmeiras', 1, 0, 1, 1, 0, 0, 0, 1, 3, 0, 0  )]
+    >>> melhor_defesa(clubes)
+    O(s) times com a defesa menos vazada é(são):
+    Flamengo
+    Sao-Paulo
     '''
-    tomados = max_gols_tomados(clubes)
-    pior_defesa: list = []
+    tomados = min_gols_tomados(clubes)
+    melhor_defesa: list = []
     for clube in clubes:
         if clube.gols_tomados == tomados:
-            pior_defesa.append(clube.nome)
-    print('O(s) times com a pior defesa é(são):')
-    for clube in pior_defesa:
+            melhor_defesa.append(clube.nome)
+    print('O(s) times com a defesa menos vazada é(são):')
+    for clube in melhor_defesa:
         print(clube)
-    
+
+def aproveitamento_anf(clube: Clube) -> float:
+    '''
+    Determina o aproveitamento de *clube* jogando como anfitrião.
+    Exemplo:
+    >>> clube = Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0, 0, 0)
+    >>> aproveitament_anf(clube)
+    1.0
+    >>> clube = Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0)
+    >>> aproveitament_anf(clube)
+    0.0
+    '''
+    if clube.jogos_anf == 0:
+        aprov = 0.0
+    else:
+        aprov = (clube.wins_anf * 3) + clube.draws_anf / (clube.jogos_anf * 3)
+    return aprov
+
+def max_aproveitamento_anf(clubes: list[Clube], i: int) -> float:
+    '''
+    Determina o maior aproveitamento como anfitrião entre os clubes de *clubes*.
+    Sendo o aproveitamento calculado como a razão entre a quanrtidade de pontos adquirida enquanto anfitrião e a quantidade de pontos possíveis como anfitrião.
+    Retorna o aproveitamento máximo.
+    Exemplo:
+    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0, 0, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0)]
+    >>> max_aproveitamento_anf(clubes)
+    1.0
+    '''
+    max_aproveitamento = aproveitamento_anf(clubes[0])
+    if i >= len(clubes):
+        max_aproveitamento = aproveitamento_anf(clubes[0]) #Sem isso deu erro, sei que isso já foi feito antes.
+    elif aproveitamento_anf(clubes[i]) > max_aproveitamento:
+        max_aproveitamento = aproveitamento_anf(clubes[i])
+    else:
+        max_aproveitamento_anf(clubes, i + 1)
+    return max_aproveitamento
+
+def melhor_anf(clubes: list[Clube]):
+    '''
+    Imprime o(s) time(s) que obtiveram o melhor aproveitamento enquanto anfitriões.
+    Exemplo:
+    >>> clubes = [Clube('Sao-Paulo', 1, 1, 0, 2, 1, 1, 2, 1, 0, 0, 0), Clube('Flamengo', 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0)]
+    >>> melhor_anf(clubes)
+    O(s) times com o melhor aproveitamento como anfitrião é(são):
+    Sao-Paulo
+    '''
+    print('O(s) times com o melhor aproveitamento como anfitrião é(são):')
+    for clube in clubes:
+        if aproveitamento_anf(clube) == max_aproveitamento_anf(clubes, 0):
+            print(clube.nome)
 
 def le_arquivo(nome: str) -> list[str]:
     '''
@@ -362,9 +430,9 @@ def main():
     ordena_clubes(clubes)
     mostra_classificacao(clubes)
     # TODO: solução da pergunta 2
-    
+    melhor_anf(clubes)
     # TODO: solução da pergunta 3
-    pior_defesa(clubes) #Precisa ser recursiva!!!!!!!!!!!!!!!!!!!
+    melhor_defesa(clubes)
 
 if __name__ == '__main__':
     main()
